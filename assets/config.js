b08b932a38d7c5551961c04deb7f08803125680b
Bimbiiim <bimayusufdh21@gmail.com>
2026-08-07 02:25:58 +0700

    Fix Supabase project URL

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://c7cfa91c-6cf5-4d0c-88ff-799402ec9c2a.supabase.co",
+  url: "https://ocxmucpjfdcpfartyxpf.supabase.co",
   anonKey: "sb_publishable_itcdyiF4dHce366JvaSZew_FWUyjOdx",
 };
